e7afa0883f2719a55457fb51e00b3add9ecd8bdd
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-03-25 10:26:06 +0000

    updated to enhanced for loop

--- QA-ArraysProjects/src/main/java/org/example/WorkingWithArrays.java
+++ QA-ArraysProjects/src/main/java/org/example/WorkingWithArrays.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class WorkingWithArrays {
     //Challenge:
 
-    //Prompt the user to input 5 integers --> need the Scanner
-    //Store each of the integers in a regular, built-in array
-    //After the storing is completed, in a separate loop, print each of the values in the array multiplied by 2
+    //Prompt the user to input 5 integers;
+    //Store each of the integers in a regular, built-in array;
+    //After the storing is completed, in a separate loop, print each of the values in the array multiplied by 2.
 
     public static void main (String[] args){
         Scanner keyboard = new Scanner(System.in);
         int[] arrayOfValues = new int[5];
-
 
         for(int i = 0; i < arrayOfValues.length; i++) {
             System.out.print("Enter an integer\t");
             arrayOfValues[i] = keyboard.nextInt();
         }//end for
 
-        for(int i = 0; i < arrayOfValues.length; i++) {
-            int result = arrayOfValues[i] * 2;
+        for (int value : arrayOfValues) {
+            int result = value * 2;
             System.out.println("Result is " + result);
         }//end for
 
