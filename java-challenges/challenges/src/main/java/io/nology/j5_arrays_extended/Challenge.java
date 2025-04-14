6f4afd15865863e5a2da9933174d1d93dfd2c228
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-04-14 22:37:38 +0100

    j5chal1 - print array corrected

--- java-challenges/challenges/src/main/java/io/nology/j5_arrays_extended/Challenge.java
+++ java-challenges/challenges/src/main/java/io/nology/j5_arrays_extended/Challenge.java
@@ -19,38 +19,46 @@ public class Challenge {
      *         67 34 89 75
      */
     public void print2DArray(int rows, int columns) {
-        int[][] array = new int[rows][columns];
+        int[][] array1 = new int[rows][columns];
 
         Random random = new Random();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                array[i][j] = random.nextInt(100);
+                array1[i][j] = random.nextInt(100);
             }
         }
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                System.out.println(array[i][j] + " ");
+                System.out.print(array1[i][j] + " ");
             }
             System.out.println();
+        }
     }
 
     /**
      * Finds and prints the maximum element in each row of a 2D array.
      *
-     * @param array the 2D array of integers
+     * @param array2 the 2D array of integers
      *              14 73 28 42
      *              39 91 19 46
      *              67 34 89 75
-     * @return the maximum element in each row of the arrat, printed one under the
+     * @return the maximum element in each row of the array, printed one under the
      *         other
      *         Example output:
      *         73
      *         91
      *         89
      */
-    public void findMaxInRows(int[][] array) {
-
-        return;
+    public void findMaxInRows(int[][] array2) {
+        for (int i = 0; i < array2.length; i++) {
+            int max = array2[i][0];
+            for (int j = 0; j < array2[i].length; j++) {
+                if (array2[i][j] > max) {
+                    max = array2[i][j];
+                }
+            }
+            System.out.println(max);
+        }
     }
 
     /**
@@ -77,3 +85,4 @@ public class Challenge {
         return;
     }
 }
+//
